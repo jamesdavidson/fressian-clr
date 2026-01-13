38ba5795bdb4de2a96efeddf2424a3669658ed85
James Davidson <jamesd3142@gmail.com>
2026-01-13 13:45:10 +0800

    Merge csproj and nuspec
    With help from https://github.com/hvanbakel/CsprojToVs2017

--- src/clr/Properties/AssemblyInfo.cs
+++ src/clr/Properties/AssemblyInfo.cs
@@ -13,18 +13,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("fressian")]
-[assembly: AssemblyDescription("A CLR port of fressian")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ThorTech Solutions")]
-[assembly: AssemblyProduct("fressian")]
-[assembly: AssemblyCopyright("Copyright © Metadata Partners, LLC.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -32,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("17ca3555-3bdc-43c1-bdf2-059537438a1d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
